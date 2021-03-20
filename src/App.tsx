eb544e784569b7c258fff50237709e78f1ea2bf1
LyhourChhen <ukamilove66@gmail.com>
2021-03-20 18:26:33 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { DatePicker } from "antd";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <DatePicker />
-    </div>
+    <Fragment>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div>
+          <h1>Hello world</h1>
+          <DatePicker />
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
